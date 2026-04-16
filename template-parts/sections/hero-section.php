6411ac63d4e96e00cf02f826a36169a28c96d072
hamzasispn <talha.sispn@gmail.com>
2026-04-16 06:08:59 +0500

    updates

--- template-parts/sections/hero-section.php
+++ template-parts/sections/hero-section.php
@@ -1,7 +1,6 @@
 <section class="h-screen relative">
     <video autoplay muted loop class="md:block hidden w-full h-full object-cover">
         <source src="<?= get_template_directory_uri() . '/assets/videos/hero-video-16-9.mp4' ?>" type="video/mp4">
-        Your browser does not support the video tag.
     </video>
     <video autoplay muted loop class="md:hidden block w-full h-full object-cover">
         <source src="<?= get_template_directory_uri() . '/assets/videos/hero-video-9-16.mp4' ?>" type="video/mp4">
